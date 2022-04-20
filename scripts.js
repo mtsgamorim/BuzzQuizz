6e837806a46c028be7da2e17b54c8cbbf8e6b14e
Amorim <mtsgamorim@gmail.com>
2022-04-20 20:46:07 -0300

    Criação do Quizz informacoes basicas concluida

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ const paginaInicial = document.querySelector(".PaginaInicial");
 const paginaQuizz = document.querySelector(".PaginaDoQuizz");
 const paginaCriacao = document.querySelector(".TerceiraCriacao");
 
+let tituloQuizz = "";
+let imgQuizz;
+let qntdPerguntas;
+let qntdNiveis;
+
+// ---------------------    AREA DOS BOTOES     ------------------------
+
 function irParaCriacaoQuizz() {
     paginaInicial.classList.add("esconder");
     paginaCriacao.classList.remove("esconder");
@@ -10,14 +17,22 @@ function irParaCriacaoQuizz() {
 
 function informacaoCriarQuizz() {
     //PEGAR INFORMAÇOES DO IMPUT E TRATA-LAS
+    tituloQuizz = document.querySelector(".input_titulo").value;
+    imgQuizz = document.querySelector(".input_url").value;
+    qntdPerguntas = document.querySelector(".input_qntdPerguntas").value;
+    qntdNiveis = document.querySelector(".input_qntdNiveisQuizz").value;
 
-
-    document.querySelector(".inicio").classList.add("esconder");
-    document.querySelector(".parte2").classList.remove("esconder");
+    if (tituloValido(tituloQuizz) && isValidHttpUrl(imgQuizz) && qntdPerguntasValido(Number(qntdPerguntas)) && qntdNivelValido(Number(qntdNiveis))) {
+        document.querySelector(".inicio").classList.add("esconder");
+        document.querySelector(".parte2").classList.remove("esconder");
+    } else {
+        alert("Você deve preencher os dados corretamente");
+    }
 }
 
 function dadosDasPerguntas() {
     //PEGAR INFORMAÇOES DO IMPUT E TRATA-LAS
+
 
 
     document.querySelector(".parte2").classList.add("esconder");
@@ -46,10 +61,11 @@ function criacaoParaHome() {
     document.querySelector(".parte4").classList.add("esconder");
     paginaInicial.classList.remove("esconder");
 }
- function paginaInicialParaQuizz() {
+
+function paginaInicialParaQuizz() {
     paginaInicial.classList.add("esconder");
     paginaQuizz.classList.remove("esconder");
- }
+}
 
 
 
@@ -58,3 +74,24 @@ function criacaoParaHome() {
 
 
 //BOTAO REINICIAR QUIZZ NÃO IMPLEMENTADO
+
+// ---------------------    AREA DOS BOTOES     ------------------------
+
+// ---------------------  Funcoes true false   ------------------------
+
+const tituloValido = tituloQuizz => tituloQuizz.length >= 20 && tituloQuizz.length <= 65;
+
+function isValidHttpUrl(string) {
+    let url;
+
+    try {
+        url = new URL(string);
+    } catch (_) {
+        return false;
+    }
+
+    return url.protocol === "http:" || url.protocol === "https:";
+}
+const qntdPerguntasValido = qntdPerguntas => qntdPerguntas >= 3;
+
+const qntdNivelValido = qntdNiveis => qntdNiveis >= 2;
